aa2b98e8422b35449f08e84122ca5a69e9afd46c
Julian Wyatt <julianwyatt@Julians-MBP-2.domain>
2020-05-21 21:21:34 -0700

    first commit

--- client/javascripts/to-do.js
+++ client/javascripts/to-do.js
@@ -1,6 +1,9 @@
 //jshint esversion: 6
-
 let controller = function() {
+  //load comments from local
+  if (localStorage.getItem("toDoList")) {
+    $(".comments").html(localStorage.getItem("toDoList"));
+  }
 
   let addCommentFromInputBox = function() {
     //Semmy uses "$" to name variables that will contain jQuery objects
@@ -12,8 +15,15 @@ let controller = function() {
       $(".comments").append($new_comment);
       //$new_comment.fadeIn();
       $(".comment-input input").val("");
+      //log the list of p elements
+      //console.log($(".comments"));
+
+      //localStorage.setItem("toDoList", $(".comments").html());
+      //console.log(localStorage.getItem("toDoList"));
+      console.log($(".comments").html());
     }
   };
+
 
   $(".comment-input button").on("click", function(event) {
     addCommentFromInputBox();
@@ -26,4 +36,17 @@ let controller = function() {
   });
 };
 
-$(document).ready(controller);
+//reload page to delete
+
+let deleteHandler = () => {
+  console.log("dH")
+  localStorage.removeItem("commentsList")
+  window.location.reload();
+}
+
+$(document).ready(() => {
+  console.log("ready")
+  let buttonElem = document.querySelectorAll('button')[1];
+  buttonElem.addEventListener('click', deleteHandler);
+  controller();
+});
